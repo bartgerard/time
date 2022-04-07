bee9a750675a2bdf9a3dc616b8fd6271cef21df3
bartgerard <bart.gerard@gmail.com>
2022-04-07 12:00:49 +0200

    First Commit

---
+++ src/test/java/be/gerard/time/TimeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.gerard.time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
