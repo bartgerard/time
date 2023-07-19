ce1064201de11fe744b3c16f2d1fc88779a5d4b3
bartgerard <bart.gerard@gmail.com>
2023-07-19 22:56:10 +0200

    :white_check_mark: Improve tests

--- src/test/groovy/be/gerard/time/DateRangeTestUtils.groovy
+++ src/test/groovy/be/gerard/time/DateRangeTestUtils.groovy
@@ -1,10 +1,7 @@
 package be.gerard.time
 
-
 import java.time.LocalDate
 import java.time.YearMonth
-
-import static org.apache.commons.lang3.Validate.notEmpty
 
 class DateRangeTestUtils {
 
@@ -32,26 +29,6 @@ class DateRangeTestUtils {
         return rangesAsString.stream()
                 .map { DateRange.parse(it) }
                 .toList()
-    }
-
-    static DateRange range1(
-            final List<String> rangeAsString
-    ) {
-        notEmpty(rangeAsString)
-
-        return DateRange.ofOneDay(day(rangeAsString.get(0)))
-    }
-
-    static DateRange range(
-            final List<String> rangeAsString
-    ) {
-        notEmpty(rangeAsString)
-
-        if (rangeAsString.size() == 1) {
-            return DateRange.startingOn(day(rangeAsString.get(0)))
-        } else {
-            return DateRange.of(day(rangeAsString.get(0)), day(rangeAsString.get(1)))
-        }
     }
 
 }
